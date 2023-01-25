c97de3e947ae3731fc1dcf37370d52f4b6f4e647
taecode <gitty58@gmail.com>
2023-01-25 10:25:45 +0900

    #Item props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Greeting from './components/Greeting';
-import FoodList from './components/FoodList';
-import Hello from './components/Hello';
+import ItemMain from './components/item/ItemMain';
 
 
 function App() {
   return (
     <>
-      <Hello/>
+      <ItemMain/>
     </>
   );
 }
